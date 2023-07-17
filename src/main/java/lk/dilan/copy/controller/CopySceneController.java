0e97a678195e8c3324350f41a87be97b46115b99
Dilan Asiri <dilanasiri95@gmail.com>
2023-07-17 13:05:24 +0530

    finalize the btnCopyOnAction() method

--- src/main/java/lk/dilan/copy/controller/CopySceneController.java
+++ src/main/java/lk/dilan/copy/controller/CopySceneController.java
@@ -1,15 +1,16 @@
 package lk.dilan.copy.controller;
 
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Optional;
 
 public class CopySceneController {
 
@@ -37,14 +38,47 @@ public class CopySceneController {
     @FXML
     private TextField txtTarget;
     private File sourceFile;
-    private File targetFile;
+    private File targetFolder;
 public void initialize(){
     btnCopy.setDisable(true);
 }
 
     @FXML
     void btnCopyOnAction(ActionEvent event) {
+        File targetFile = new File(targetFolder, sourceFile.getName());
+        if (targetFile.exists()){
+            Optional<ButtonType> optResult = new Alert(Alert.AlertType.CONFIRMATION, "File already exists, are you sure to replace the file?", ButtonType.YES, ButtonType.NO).showAndWait();
+            if (optResult.isEmpty() || optResult.get()==ButtonType.NO) return;
+        }
+        btnCopy.getScene().getWindow().setHeight(325);
+        Task task=new Task() {
+            @Override
+            protected Object call() throws Exception {
+                FileInputStream fis = new FileInputStream(sourceFile);
+                FileOutputStream fos = new FileOutputStream(targetFile);
 
+                double write=0.0;
+                while (true){
+                    byte[] buffer = new byte[1024 * 10];
+                    int read = fis.read(buffer);
+                    if (read==-1) break;
+                    fos.write(buffer,0,read);
+                    write +=read;
+                    updateMessage(String.format("%2.2f",write/sourceFile.length() *100).concat("% Complete"));
+                    updateProgress(write,sourceFile.length());
+                }
+                fis.close();
+                fos.close();
+                return null;
+            }
+        };
+        task.exceptionProperty().addListener(observable -> {
+            task.getException().printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"Something went wrong,please try again!!!").show();
+        });
+        lblStatus.textProperty().bind(task.messageProperty());
+        prgCopy.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
     }
 
     @FXML
@@ -58,17 +92,17 @@ public void initialize(){
     }
 
     private void enableCopyButton() {
-        btnCopy.setDisable(sourceFile==null || targetFile==null || sourceFile.getParentFile().equals(targetFile));
+        btnCopy.setDisable(sourceFile==null || targetFolder ==null || sourceFile.getParentFile().equals(targetFolder));
     }
 
     @FXML
     void btnTargetOnAction(ActionEvent event) {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Select the destination folder");
-        targetFile=directoryChooser.showDialog(btnTarget.getScene().getWindow());
+        targetFolder =directoryChooser.showDialog(btnTarget.getScene().getWindow());
         enableCopyButton();
-        if (targetFile==null) return;
-        txtTarget.setText(targetFile.getAbsolutePath());
+        if (targetFolder ==null) return;
+        txtTarget.setText(targetFolder.getAbsolutePath());
     }
 
     @FXML
