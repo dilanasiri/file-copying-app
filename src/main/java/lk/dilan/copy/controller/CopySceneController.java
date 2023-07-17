0a724c89fc29e09663b9e583c6228c0f101eadda
Dilan Asiri <dilanasiri95@gmail.com>
2023-07-17 11:22:29 +0530

    finalize some css and btnOnAction() method

--- src/main/java/lk/dilan/copy/controller/CopySceneController.java
+++ src/main/java/lk/dilan/copy/controller/CopySceneController.java
@@ -6,6 +6,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class CopySceneController {
 
@@ -32,6 +35,7 @@ public class CopySceneController {
 
     @FXML
     private TextField txtTarget;
+    private File sourceFile;
 
     @FXML
     void btnCopyOnAction(ActionEvent event) {
@@ -40,11 +44,21 @@ public class CopySceneController {
 
     @FXML
     void btnSourceOnAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a file to copy");
+        sourceFile=fileChooser.showOpenDialog(btnCopy.getScene().getWindow());
+        enableCopyButton();
+        if (sourceFile==null) return;
+        txtSource.setText(sourceFile.getAbsolutePath());
+    }
 
+    private void enableCopyButton() {
+        btnCopy.setDisable(sourceFile==null);
     }
 
     @FXML
     void btnTargetOnAction(ActionEvent event) {
+
 
     }
 
